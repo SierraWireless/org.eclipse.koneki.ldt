b2f6d163d4c0d86821a3b7379e0c0f6870534e2f
Benjamin Cabé <BCabe@sierrawireless.com>
2012-07-16 00:19:10 +0200

    Bug 385130 - Provide basic syntax highlight for Markdown in Luadocumentor comments

--- plugins/org.eclipse.koneki.ldt.ui/src/org/eclipse/koneki/ldt/ui/internal/editor/text/LuaDocumentorScanner.java
+++ plugins/org.eclipse.koneki.ldt.ui/src/org/eclipse/koneki/ldt/ui/internal/editor/text/LuaDocumentorScanner.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.koneki.ldt.ui.internal.editor.text;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -19,9 +20,15 @@ import org.eclipse.dltk.ui.text.ScriptCommentScanner;
 import org.eclipse.dltk.ui.text.ScriptSourceViewerConfiguration;
 import org.eclipse.dltk.ui.text.TodoTaskPreferencesOnPreferenceStore;
 import org.eclipse.dltk.ui.text.rules.CombinedWordRule.WordMatcher;
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.IWhitespaceDetector;
 import org.eclipse.jface.text.rules.IWordDetector;
+import org.eclipse.jface.text.rules.PatternRule;
+import org.eclipse.jface.text.rules.Token;
 import org.eclipse.koneki.ldt.ui.internal.editor.LuaDocumentorTags;
+import org.eclipse.swt.SWT;
 
 public class LuaDocumentorScanner extends ScriptCommentScanner {
 
@@ -40,6 +47,36 @@ public class LuaDocumentorScanner extends ScriptCommentScanner {
 		final List<WordMatcher> matchers = super.createMatchers();
 		matchers.add(createLuaDocumentorKeywordMatcher());
 		return matchers;
+	}
+
+	@Override
+	protected List<IRule> createRules() {
+		final List<IRule> rules = new ArrayList<IRule>();
+		Token luaDoc = getToken(ILuaColorConstants.LUA_DOC);
+		TextAttribute textAttribute;
+
+		// Rules below are used to perform basic MarkDown pattern detection (bold, italic, ...)
+		Token monospace = new Token(luaDoc.getData());
+		textAttribute = (TextAttribute) monospace.getData();
+		monospace.setData(new TextAttribute(textAttribute.getForeground(), textAttribute.getBackground(), textAttribute.getStyle()
+				| TextAttribute.UNDERLINE));
+		rules.add(new PatternRule("`", "`", monospace, (char) 0, false)); //$NON-NLS-1$//$NON-NLS-2$
+
+		Token bold = new Token(luaDoc.getData());
+		textAttribute = (TextAttribute) bold.getData();
+		bold.setData(new TextAttribute(textAttribute.getForeground(), textAttribute.getBackground(), textAttribute.getStyle() | SWT.BOLD));
+		rules.add(new PatternRule("__", "__", bold, (char) 0, false)); //$NON-NLS-1$//$NON-NLS-2$
+		rules.add(new PatternRule("**", "**", bold, (char) 0, false)); //$NON-NLS-1$//$NON-NLS-2$
+
+		Token italic = new Token(luaDoc.getData());
+		textAttribute = (TextAttribute) italic.getData();
+		italic.setData(new TextAttribute(textAttribute.getForeground(), textAttribute.getBackground(), textAttribute.getStyle() | SWT.ITALIC));
+		rules.add(new PatternRule("_", "_", italic, (char) 0, false)); //$NON-NLS-1$//$NON-NLS-2$
+		rules.add(new PatternRule("*", "*", italic, (char) 0, false)); //$NON-NLS-1$//$NON-NLS-2$
+
+		rules.addAll(super.createRules());
+
+		return rules;
 	}
 
 	private WordMatcher createLuaDocumentorKeywordMatcher() {
@@ -98,6 +135,22 @@ public class LuaDocumentorScanner extends ScriptCommentScanner {
 				return c == '.' || c == '-' || Character.isJavaIdentifierPart(c);
 			}
 		};
+	}
+
+	@Override
+	public IToken nextToken() {
+		fTokenOffset = fOffset;
+		fColumn = UNDEFINED;
+		if (fRules != null) {
+			for (int i = 0; i < fRules.length; i++) {
+				IToken token = (fRules[i].evaluate(this));
+				if (!token.isUndefined())
+					return token;
+			}
+		}
+		if (read() == EOF)
+			return Token.EOF;
+		return fDefaultReturnToken;
 	}
 
 }
