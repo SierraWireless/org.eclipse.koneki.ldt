18b976d762b8c1a6085aa7ddd94f8a2d0321d3af
sbernard <sbernard@sierrawireless.com>
2012-09-03 16:13:29 +0200

    backport splahhandling

--- plugins/org.eclipse.koneki.ldt.branding/src/org/eclipse/koneki/ldt/branding/Activator.java
+++ plugins/org.eclipse.koneki.ldt.branding/src/org/eclipse/koneki/ldt/branding/Activator.java
@@ -11,6 +11,16 @@
 
 package org.eclipse.koneki.ldt.branding;
 
+import java.util.Iterator;
+
+import org.eclipse.equinox.p2.engine.IProfileRegistry;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.metadata.Version;
+import org.eclipse.equinox.p2.operations.ProvisioningSession;
+import org.eclipse.equinox.p2.query.IQueryResult;
+import org.eclipse.equinox.p2.query.IQueryable;
+import org.eclipse.equinox.p2.query.QueryUtil;
+import org.eclipse.equinox.p2.ui.ProvisioningUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -18,6 +28,8 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.koneki.ldt.branding"; //$NON-NLS-1$
+
+	public static final String PRODUCT_VERSION_PROPERTY = "product.version"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
@@ -36,6 +48,47 @@ public class Activator extends AbstractUIPlugin {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		// get version of Product installable unit !
+		String version = "NA"; //$NON-NLS-1$
+		System.setProperty(PRODUCT_VERSION_PROPERTY, version);
+
+		ProvisioningUI provisioningUI = ProvisioningUI.getDefaultUI();
+		if (null == provisioningUI) {
+			return;
+		}
+
+		ProvisioningSession provisioningSession = provisioningUI.getSession();
+		if (null == provisioningSession) {
+			return;
+		}
+
+		String profileId = provisioningUI.getProfileId();
+		IQueryable<IInstallableUnit> queryable = ((IProfileRegistry) provisioningSession.getProvisioningAgent().getService(
+				IProfileRegistry.SERVICE_NAME)).getProfile(profileId);
+		if (null == queryable) {
+			return;
+		}
+
+		String pId = "org.eclipse.koneki.ldt.product-product"; //$NON-NLS-1$
+
+		if (null != queryable) {
+			IQueryResult<IInstallableUnit> iqr = queryable.query(QueryUtil.createIUQuery(pId), null);
+			if (null != iqr) {
+				Iterator<IInstallableUnit> ius = iqr.iterator();
+				if (ius.hasNext()) {
+					IInstallableUnit iu = ius.next();
+					Version v = iu.getVersion();
+
+					if (null != v) {
+						version = v.toString();
+					}
+				}
+			}
+		}
+
+		// set the version as system properties
+		System.setProperty(PRODUCT_VERSION_PROPERTY, version);
+
 		plugin = this;
 	}
 
--- plugins/org.eclipse.koneki.ldt.branding/src/org/eclipse/koneki/ldt/branding/splash/internal/SplashHandler.java
+++ plugins/org.eclipse.koneki.ldt.branding/src/org/eclipse/koneki/ldt/branding/splash/internal/SplashHandler.java
@@ -14,6 +14,7 @@ package org.eclipse.koneki.ldt.branding.splash.internal;
 import org.eclipse.core.runtime.IProduct;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.StringConverter;
+import org.eclipse.koneki.ldt.branding.Activator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
@@ -32,11 +33,16 @@ import org.eclipse.ui.splash.BasicSplashHandler;
  */
 public class SplashHandler extends BasicSplashHandler {
 
+	private static final int DEFAULT_FOREGROUND_COLOR = 0xD2D7FF;
+	private static final Rectangle DEFAULT_MESSAGE_RECT = new Rectangle(1, 1, 10, 100);
+	private static final Rectangle DEFAULT_PROGRESS_RECT = new Rectangle(1, 12, 10, 100);
+
 	private Font font;
 
 	public void init(Shell splash) {
 		super.init(splash);
 
+		// get product information
 		String progressRectString = null;
 		String messageRectString = null;
 		String foregroundColorString = null;
@@ -46,36 +52,38 @@ public class SplashHandler extends BasicSplashHandler {
 			messageRectString = product.getProperty(IProductConstants.STARTUP_MESSAGE_RECT);
 			foregroundColorString = product.getProperty(IProductConstants.STARTUP_FOREGROUND_COLOR);
 		}
-		Rectangle progressRect = StringConverter.asRectangle(progressRectString, new Rectangle(10, 150, 100, 15));
-		setProgressRect(progressRect);
 
-		Rectangle messageRect = StringConverter.asRectangle(messageRectString, new Rectangle(10, 35, 300, 15));
+		// set message position
+		Rectangle messageRect = StringConverter.asRectangle(messageRectString, DEFAULT_MESSAGE_RECT);
 		setMessageRect(messageRect);
 
+		// set progress position
+		Rectangle progressRect = StringConverter.asRectangle(progressRectString, DEFAULT_PROGRESS_RECT);
+		setProgressRect(progressRect);
+
+		// get foreground color
 		int foregroundColorInteger;
 		try {
 			foregroundColorInteger = Integer.parseInt(foregroundColorString, 16);
 		} catch (NumberFormatException ex) {
-			foregroundColorInteger = 0xD2D7FF; // off white
+			foregroundColorInteger = DEFAULT_FOREGROUND_COLOR;
 		}
-
 		setForeground(new RGB((foregroundColorInteger & 0xFF0000) >> 16, (foregroundColorInteger & 0xFF00) >> 8, foregroundColorInteger & 0xFF));
 
-		String version = (String) Platform.getBundle("org.eclipse.koneki.ldt").getHeaders().get(org.osgi.framework.Constants.BUNDLE_VERSION); //$NON-NLS-1$
-		// version = version.substring(0, 3);
-		//final String buildId = System.getProperty("eclipse.buildId", "Version : " + version); //$NON-NLS-1$ //$NON-NLS-2$
+		String version = System.getProperty(Activator.PRODUCT_VERSION_PROPERTY);
+		int lastIndexOf = version.lastIndexOf('.');
+		if (lastIndexOf != -1)
+			version = version.substring(0, lastIndexOf);
 
-		final String buildId = "Version: " + version;
-		String buildIdLocString = product.getProperty("buildIdLocation"); //$NON-NLS-1$
-		final Point buildIdPoint = StringConverter.asPoint(buildIdLocString, new Point(280, 150));
+		final String buildId = "Version: " + version; //$NON-NLS-1$
+		final Point position = new Point(280, 150);
 
-		font = new Font(Display.getDefault(), new FontData("Helvetica", (Platform.getOS().equals(Platform.OS_MACOSX) ? 9 : 7), SWT.BOLD));
+		font = new Font(Display.getDefault(), new FontData("Helvetica", (Platform.getOS().equals(Platform.OS_MACOSX) ? 9 : 7), SWT.BOLD)); //$NON-NLS-1$
 		getContent().addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
 				e.gc.setForeground(getForeground());
 				e.gc.setFont(font);
-				// e.gc.setAntialias(SWT.ON);
-				e.gc.drawText(buildId, buildIdPoint.x, buildIdPoint.y, true);
+				e.gc.drawText(buildId, position.x, position.y, true);
 			}
 		});
 	}
